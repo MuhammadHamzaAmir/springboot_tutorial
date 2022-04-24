c7eca42e038c025f010d6c20de950a62647c3e11
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-04-25 00:02:54 +0500

    Config file added

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student minahil = new Student("Minahil",21, LocalDate.of(2000, Month.MAY,20),"mk@gmail.com");
+            Student fatima = new Student("Fatima",21, LocalDate.of(2000, Month.FEBRUARY,20),"fg@gmail.com");
+
+            repository.saveAll(List.of(minahil,fatima));
+
+        };
+    }
+}
